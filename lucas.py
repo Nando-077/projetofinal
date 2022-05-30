1b8c39a4337804983ff1953584ad85eff4cd0df0
LucasEndo03 <endo.lucas@gmail.com>
2022-05-29 23:44:50 -0300

    Tela inicial

--- lucas.py
+++ lucas.py
@@ -11,15 +11,20 @@ game = True
 # ------ Carregar a mesa da roleta
 mesa_1 = pygame.image.load('mesafinal.png').convert()
 mesa_2 = pygame.transform.scale(mesa_1, (1000, 500))
+
 # ------ Carregar a roleta
 roleta_1 = pygame.image.load('roletafinal_.gif').convert()
 roleta_2 = pygame.transform.scale(roleta_1,(250,250))
+
 # Base do texto
 base1 = pygame.image.load('basef.png').convert()
 base2 = pygame.transform.scale(base1,(1000,150))
+
 # Texto da tela inicial
 font = pygame.font.SysFont(None, 30)
-text = font.render('Seja bem-vindo a ROLETA', True, (0, 0, 0))
+text = font.render('Seja bem-vindo a roleta', True, (0, 0, 0))
+text2 = font.render('Na roleta você pode apostar em uma COR (2x) ou em um NÚMERO (10x)', True,(0,0,0))
+text3 = font.render('Aperte a BARRA DE ESPAÇO para começar!', True,(0,0,0))
 
 # ===== Loop principal =====
 while game:
@@ -34,7 +39,11 @@ while game:
     window.blit(mesa_2, (0, 0))
     window.blit(roleta_2, (65,117))
     window.blit(base2,(0,500))
+
+    #------ TEXTo
     window.blit(text, (25, 520))
+    window.blit(text2,(25,545))
+    window.blit(text3,(25,590))
    
     # ----- Atualiza estado do jogo
     pygame.display.update()  # Mostra o novo frame para o jogador
